b8d906ab60bb5e6c2ddfc6ce0ee5e9f42a251d89
morejavac <2993216343@qq.com>
2018-05-09 00:05:17 +0800

    Initial commit

---
+++ app/src/main/java/com/wac/cp/wac/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wac.cp.wac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
